fbec10b9407d9d8b50f92cd6ced8cf84205b7094
Jack Miner <3ch01c@gmail.com>
2019-05-21 12:17:37 -0600

    One-lined docker install and chmodded docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 # Install docker
 DOCKER_VERSION="test" # use "get" for stable version
-curl -fsSL https://$DOCKER_VERSION.docker.com -o install-docker.sh
-sh install-docker.sh
+curl -fsSL https://$DOCKER_VERSION.docker.com | sh -
 # Install docker-compose
 DOCKER_COMPOSE_VERSION="1.24.0"
-sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
