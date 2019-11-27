e394704249f34b1caef3d532cc9e16806b9fa589
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2019-11-27 14:50:15 -0700

    Updated docker install script

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+# Install Docker, docker-compose, and add the current user to the docker group.
+
+DOCKER_VERSION="get" # use "test" for latest (possibly unstable) version
+DOCKER_COMPOSE_VERSION="1.25.0"
+
 # Install docker
-DOCKER_VERSION="test" # use "get" for stable version
 curl -fsSL https://$DOCKER_VERSION.docker.com | sh -
+
 # Install docker-compose
-DOCKER_COMPOSE_VERSION="1.24.0"
 sudo curl -fsSL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Add user to docker group
+sudo usermod -aG docker $(whoami)
+# Make docker the user's current group (so they don't have to logout and back in)
+newgrp docker
